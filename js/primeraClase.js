89f21b3222d60933818c7a2673ae094f4ea6f489
Candela Mercado <mercadocandela16@gmail.com>
2022-08-28 13:36:56 -0300

    primer commit

---
+++ js/primeraClase.js
@@ -0,0 +1,29 @@
+console.log ("hola mundo");
+document.write ("Hola mundo") ;
+
+console.log("Este es un 2do mensaje");
+
+
+console.log (typeof PI);
+
+let suma = "01234";
+console.log ("valorDeEntrada");
+console.log (typeof valorDeEntrada)
+
+suma =  suma + valorDeEntrada;
+
+valorDeEntrada = Number (prompt ("indique un valor"));
+console.log("valorDeEntrada")
+
+suma =  suma + valorDeEntrada;
+
+valorDeEntrada = Number (prompt ("indique un valor"));
+console.log("valorDeEntrada")
+
+suma =  suma + valorDeEntrada;
+
+valorDeEntrada = Number (prompt ("indique un valor"));
+console.log("valorDeEntrada")
+
+suma =  suma + valorDeEntrada;
+console.log ("La suma es de"+suma)
